c666284b731cd7dce459d23c48e2aafc3ea27be2
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-11-25 20:36:08 +0100

    feat: allow dynamic grid size creation

--- javascript.js
+++ javascript.js
@@ -1,13 +1,17 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#button");
 
 console.log(container);
-function createDivGrid () {
-    for (let i=0; i<16; i++) {
+function createDivGrid (squaresPerSide) {
+    const squareSize = container.clientWidth / squaresPerSide;
+    const newDivsArray = []
+    for (let i=0; i<squaresPerSide*squaresPerSide; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
-        div.textContent = "div";
-        container.appendChild(div);
+        div.style.cssText = `width: ${squareSize}px; height: ${squareSize}px`;
+        newDivsArray.push(div)
     }
+    container.replaceChildren(...newDivsArray);
 }
 
 function hoverChangeColor (event) {
@@ -15,6 +19,13 @@ function hoverChangeColor (event) {
     console.log(event);
 }
 
-createDivGrid();
+function resetGrid (event) {
+    let squaresPerSide = prompt("Please enter required number of squares per side.");
+    if (squaresPerSide > 100) { squaresPerSide = 100; }
+    createDivGrid(squaresPerSide);
+}
+
+createDivGrid(4);
 
 container.addEventListener("mouseover", hoverChangeColor);
+button.addEventListener("click", resetGrid)
