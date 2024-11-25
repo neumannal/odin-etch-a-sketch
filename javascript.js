13609d539b8924f4d59e193c5c12bdfa0c5fba96
Alexander Neumann <181498776+neumannal@users.noreply.github.com>
2024-11-25 19:44:16 +0100

    refactor: move style to css

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
-function createDivGrid () {
-    const container = document.querySelector("#container");
+const container = document.querySelector("#container");
 
-    container.style.cssText = "display: flex; flex-wrap: wrap; width: 45vw"
+console.log(container);
+function createDivGrid () {
     for (let i=0; i<16; i++) {
         const div = document.createElement("div");
-        div.id = "square";
+        div.classList.add("square");
         div.textContent = "div";
-        div.style.cssText = "width: 10vw; height: 10vw; background-color: blue";
         container.appendChild(div);
     }
 }
